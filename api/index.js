4bd34bd16bdba51591bab76e4e84a1b5fa28dddf
Thomas Maximiliano Mintzer <111892223+ThoMintz@users.noreply.github.com>
2024-04-03 14:04:30 -0300

    cambios en index.js

--- api/index.js
+++ api/index.js
@@ -1,15 +1,15 @@
+import series from "./series.json";
 const express = require("express");
 const app = express();
 const path = require("path");
 const port = process.env.PORT || 3001; // Utilizamos process.env.PORT para permitir que Vercel asigne un puerto automáticamente
 
+
 // Configuramos Express para servir archivos estáticos desde la carpeta "public"
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Configuramos una ruta para servir el archivo HTML desde la carpeta "cliente_servidor"
-app.get("/cliente_servidor", (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'cliente_servidor', 'index.html'));
-});
+app.get("/api/series", (req, res) => res.send(series));
 
 // Configuramos una ruta de ejemplo
 app.get("/express", (req, res) => {
